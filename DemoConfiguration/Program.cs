a5942256610804b3b70495d8aa2c7bf0fae6806a
Tanus <romain@avonde.net>
2019-02-04 20:39:13 +0100

    Ajout de la présentation et de la partie configuration.

---
+++ DemoConfiguration/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace DemoConfiguration
+{
+    #region Usings
+
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Threading.Tasks;
+
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+
+    #endregion
+
+    internal static class Program
+    {
+        #region Champs et constantes statiques
+
+        public static Dictionary<string, string> arrayDict = new Dictionary<string, string>
+        {
+            { "0", "value0" },
+            { "1", "value1" },
+            { "2", "value2" },
+            { "4", "value4" },
+            { "5", "value5" }
+        };
+
+        #endregion
+
+        #region Méthodes privées
+
+        private static async Task Main(string[] args)
+        {
+            var host = new HostBuilder()
+                       .ConfigureHostConfiguration(configHost =>
+                       {
+                           configHost.SetBasePath(Directory.GetCurrentDirectory());
+                           configHost.AddJsonFile("hostsettings.json", true);
+                           configHost.AddEnvironmentVariables("PREFIX_");
+                           configHost.AddCommandLine(args);
+                       })
+                       .ConfigureAppConfiguration((hostContext, configApp) =>
+                       {
+                           configApp.SetBasePath(Directory.GetCurrentDirectory());
+                           configApp.AddInMemoryCollection(arrayDict);
+                           configApp.AddJsonFile("appsettings.json", true);
+                           configApp.AddJsonFile(
+                               $"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json",
+                               true);
+                           configApp.AddEnvironmentVariables("PREFIX_");
+                           configApp.AddCommandLine(args);
+                       })
+                       .Build();
+
+            var sections = host.Services.GetService<IConfiguration>().GetChildren();
+
+            await host.RunAsync();
+        }
+
+        #endregion
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+xx-02-2019 - Concepts du .net core et intro au .net core 3.0
